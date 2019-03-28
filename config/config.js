49197a0cb8a8f75b75b02d7d45fd66d548cf6b15
Ritchie Huang <ritchiehuang@promote.cache-dns.local>
2019-03-28 16:26:50 +0800

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+const config = {
+  // debug 为 true 时，用于本地调试
+  debug: true,
+  apiHost: 'http://127.0.0.1:8080/', //
+  description: 'CNode：Node.js专业中文社区', // 社区的描述
+  keywords: 'nodejs, node, express, connect, socket.io'
+
+  // 其他配置项...
+}
+module.exports = config
